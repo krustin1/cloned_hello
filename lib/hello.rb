378ecaf4928be8d95f72928febde5be810d74426
krustin1 <kelseyrustin@hotmail.com>
2020-10-09 17:43:05 -0400

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Kelsey Rustin (kelsey@somewhere.com)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
